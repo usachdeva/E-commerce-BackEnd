f46fbaf831cee006f694c0473a24279fb5ef71b3
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-05-29 21:06:32 -0400

    tags-routes working

--- models/index.js
+++ models/index.js
@@ -9,9 +9,11 @@ Product.belongsTo(Category, {
     foreignKey: "category_id",
     onDelete: "CASCADE",
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
     foreignKey: "category_id",
+    onDelete: "CASCADE",
 });
 
 // Products belongToMany Tags (through ProductTag)
@@ -19,6 +21,7 @@ Product.belongsToMany(Tag, {
     through: { model: ProductTag, unique: false },
     as: "product_tagged",
 });
+
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
     through: { model: ProductTag, unique: false },
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,13 +9,16 @@ router.get("/", async (req, res) => {
     // be sure to include its associated Category and Tag data
     try {
         const productData = await Product.findAll({
-            include: [{ model: Category }, { model: Tag }],
+            include: [
+                { model: Category },
+                { model: Tag, as: "product_tagged" },
+            ],
         });
 
-        if (!productData) {
-            res.status(404).json({ message: "No product found!" });
-            return;
+        if (!productData || productData.length === 0) {
+            return res.status(404).json({ message: "No products found" });
         }
+
         res.status(200).json(productData);
     } catch (err) {
         res.status(500).json(err);
@@ -28,7 +31,10 @@ router.get("/:id", async (req, res) => {
     // be sure to include its associated Category and Tag data
     try {
         const productData = await Product.findByPk(req.params.id, {
-            include: [{ model: Category }, { model: ProductTag }],
+            include: [
+                { model: Category },
+                { model: Tag, as: "product_tagged" },
+            ],
         });
 
         if (!productData) {
