57e2ba3903aa016dec6ddb1f918bbb234b9b93de
Udit Sachdeva <uditsachdeva5@gmail.com>
2024-06-06 15:16:13 -0400

    tag-routes.js file updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,15 +38,60 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    // create a new tag
+    // body should look like this
+    /*
+    {
+        tag_name: "yoyy";
+    }
+        */
+
+    try {
+        const tagData = await Tag.create(req.body);
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
 
 router.put("/:id", async (req, res) => {
     // update a tag's name by its `id` value
+    try {
+        const updatedTag = await Tag.update(
+            {
+                tag_name: req.body.tag_name,
+            },
+            {
+                where: { id: req.params.id },
+            }
+        );
+
+        res.json(updatedTag);
+    } catch (err) {
+        console.error("error:", err);
+        res.status(400).json(err);
+    }
 });
 
 router.delete("/:id", async (req, res) => {
     // delete on tag by its `id` value
+    try {
+        const tagData = await Tag.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!tagData) {
+            res.status(404).json({
+                message: "No tag found with this id!",
+            });
+            return;
+        }
+
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
